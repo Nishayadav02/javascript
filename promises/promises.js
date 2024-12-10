c2a8ee810484c93d175e75c902e82e066e4b99dc
Nishayadav02 <nishayadav5752@gmail.com>
2024-12-10 21:10:07 +0530

    code

---
+++ promises/promises.js
@@ -0,0 +1,25 @@
+/*const promiseOne = new Promise(function(resolve , reject) {
+    //Do an async task
+    //DB calls , cryptography , network
+    setTimeout(function() {
+        console.log('Async rask is complete');
+        resolve()
+        
+    }, 1000)
+})
+
+promiseOne.then(function() {
+    console.log("promise consumed");
+    
+})
+*/
+
+new Promise(function(resolve , reject) {
+    setTimeout(function() {
+        console.log("Async task 2");
+        resolve()
+    } , 1000)
+}).then(function() {
+    console.log("Async 2 resolved");
+    
+})
